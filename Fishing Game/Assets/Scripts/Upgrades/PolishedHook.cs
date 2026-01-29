784c583f33d9dee364eba7342c62922b0d2d4cf4
Anthony Thai <anthonythai07@gmail.com>
2026-01-28 23:03:30 -0500

    Started upgrade framework

---
+++ Fishing Game/Assets/Scripts/Upgrades/PolishedHook.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "PolishedHook", menuName = "Upgrades/Polished Hook")]
+public class PolishedHook : Upgrade
+{
+    [Header("Upgrade Values")]
+    public float moneyBonus;
+
+    public override void OnEquip()
+    {
+
+    }
+
+    public override void OnUnequip()
+    {
+
+    }
+}
