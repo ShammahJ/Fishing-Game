1d231935ae18d65d138a298b53ea5392c1e0120c
Anthony Thai <anthonythai07@gmail.com>
2026-02-21 18:42:04 -0500

    Finished shop ui

--- Fishing Game/Assets/Scripts/Shop/ShopSlotHover.cs
+++ Fishing Game/Assets/Scripts/Shop/ShopSlotHover.cs
@@ -8,6 +8,8 @@ public class ShopSlotHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHa
     [SerializeField] private float animationDuration = 0.2f;
     [SerializeField] private Image upgradeGlow;
 
+    private RectTransform glowRectTransform;
+
     private Upgrade assignedUpgrade;
     private ShopManager shopManager;
     private RectTransform rectTransform;
@@ -15,30 +17,59 @@ public class ShopSlotHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHa
     private void Awake()
     {
         rectTransform = GetComponent<RectTransform>();
+        glowRectTransform = upgradeGlow.GetComponent<RectTransform>();
+    }
+
+    private void Update()
+    {
+        upgradeGlow.rectTransform.Rotate(0, 0, -45f * Time.deltaTime);
     }
 
     public void Initialize(Upgrade upgrade, ShopManager manager)
     {
         assignedUpgrade = upgrade;
         shopManager = manager;
+
+        if (upgradeGlow != null && assignedUpgrade != null)
+        {
+            upgradeGlow.color = GetRarityColor(assignedUpgrade.rarity);
+        }
     }
 
     public void OnPointerEnter(PointerEventData eventData)
     {
-        StartCoroutine(ScaleTo(100f));
+        StartCoroutine(ScaleTo(rectTransform, 100f));
         if (assignedUpgrade != null && shopManager != null)
             shopManager.ShowTooltip(assignedUpgrade);
-        upgradeGlow.rectTransform.Rotate(Vector3.forward * 90f * Time.deltaTime);
+        StartCoroutine(ScaleTo(glowRectTransform, 150f));
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
-        StartCoroutine(ScaleTo(75f));
+        StartCoroutine(ScaleTo(rectTransform, 75f));
         if (shopManager != null)
             shopManager.HideTooltip();
+        StartCoroutine(ScaleTo(glowRectTransform, 0f));
     }
 
-    private IEnumerator ScaleTo(float targetSize)
+    private Color GetRarityColor(Upgrade.UpgradeRarity rarity)
+    {
+        switch (rarity)
+        {
+            case Upgrade.UpgradeRarity.Common:
+                return new Color(0.68f, 0.94f, 0.698f);
+            case Upgrade.UpgradeRarity.Rare:
+                return new Color(0.4f, 0.855f, 0.937f);
+            case Upgrade.UpgradeRarity.Epic:
+                return new Color(0.75f, 0.545f, 0.953f);
+            case Upgrade.UpgradeRarity.Legendary:
+                return new Color(0.953f, 0.878f, 0.474f);
+            default:
+                return Color.white;
+        }
+    }
+
+    private IEnumerator ScaleTo(RectTransform rectTransform, float targetSize)
     {
         Vector2 startSize = rectTransform.sizeDelta;
         Vector2 targetSizeDelta = new Vector2(targetSize, targetSize);
