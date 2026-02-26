fa3fcbcf6830c08a4335dbb1e36bbe7c43a6a8ec
TromboneonaDesk <144817218+TromboneonaDesk@users.noreply.github.com>
2026-02-25 22:39:37 -0500

    fix to DestroyOutOfBounds.cs.

--- Fishing Game/Assets/Scripts/DestroyOutOfBounds.cs
+++ Fishing Game/Assets/Scripts/DestroyOutOfBounds.cs
@@ -4,22 +4,17 @@ public class DestroyOutOfBounds : BubbleSpawner
 {
     private float topBound = 30.0f;
     private float lowerBound = -10.0f;
-    void Start()
-    {
-
-    }
-
     void Update()
     {
         if (transform.position.y > topBound)
         {
             Destroy(gameObject);
-            BubbleSpawner._totalBubbles--;
+            BubbleSpawner.TotalBubbles--;
         }
         else if (transform.position.y < lowerBound)
         {
             Destroy(gameObject);
-            BubbleSpawner._totalBubbles--;
+            BubbleSpawner.TotalBubbles--;
         }
 
 
